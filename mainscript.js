0b78b599afc8a345e520dbbdd0fe9871f3bcb9b3
Rebecca Botha <rebeccabotha@Rebeccas-MacBook-Air.local>
2017-09-15 18:14:32 +0100

    add initial calculator javascript functionality

---
+++ mainscript.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+
+  // create array including all buttons except special characters AC CE and =
+  var computeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '/', '-', '*']
+
+  //set up array to hold the calculation that needs to be done
+  var currentStatus = [];
+
+  //create callbacks for all buttons in the array. Invoke immediately
+  (function createCallbacks(computeArray) {
+    for (var i = 0; i < computeArray.length; i++) {
+    document.getElementById(computeArray[i]).addEventListener('click', createCall, false);
+    }
+  }
+  (computeArray));
+
+  function createCall(e) {
+    console.log('here');
+    currentStatus.push(e.srcElement.id);
+    console.log(currentStatus);
+  }
+
+  //create callback for equals button
+  var result;
+  document.getElementById('=').addEventListener('click', equals, false);
+
+  function equals(e) {
+    var sum = currentStatus.join("");
+    result = (eval(sum)).toString();
+    document.getElementById('display').innerHTML = result;
+    currentStatus = [result];
+  }
+
+}
